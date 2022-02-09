0c321538b174e5b81bb5f28edb84336d7fd413d8
samuel <samuelmmanor@gmail.com>
2022-02-09 10:04:15 -0500

    Completed basic deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/listings")
+    .then ((r) => r.json())
+    .then((listingArray) => setListings(listingArray))
+  }, []);
+
+  function handleDeleteListing(listingToDelete) {
+    const updatedListings = listings.filter((listing) => listing.id !== listingToDelete.id);
+    setListings(updatedListings);
+  }
+
+  const displayedListings = listings.filter((listing) => listing.description.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch} />
+      <ListingsContainer listings={displayedListings} onDeleteListing={handleDeleteListing} />
     </div>
   );
 }
